a8cd3f494e590d1645537dec3a49d72912f29d46
Colin Kern <ckern101@gmail.com>
2022-03-03 20:21:47 -0600

    Add project files.

---
+++ MoveFilesBackup/DirectoryMusic.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text.RegularExpressions;
+
+namespace MoveFilesBackup
+{
+    class DirectoryMusic
+    {
+        Regex regexSongFileEndings = new Regex(@"(\..{3,4})$");
+
+        //Walks directory and returns list of all files and folders
+        public List<string> WalkDirectory(string currentDirectory)
+        {
+            List<string> currentDirectoryFiles = new List<string>();
+
+            foreach (string files in Directory.EnumerateFileSystemEntries(currentDirectory, "*.*", SearchOption.AllDirectories))
+            {
+                string noSourceDir = files.Remove(0, currentDirectory.Length);
+                currentDirectoryFiles.Add(noSourceDir);
+            }
+
+            return currentDirectoryFiles;
+        }
+
+        //Create folders by finding list objects with no extensions
+        public void CreateFolders(List<string> directoryFiles, string targetDirectory)
+        {
+            foreach (string missingFiles in directoryFiles)
+            {
+                if (!regexSongFileEndings.IsMatch(missingFiles))
+                {
+                    Directory.CreateDirectory((targetDirectory + missingFiles));
+                    Console.WriteLine(missingFiles);
+                }
+            }
+
+        }
+
+        //Copies files to the correct folder in the target directory
+        public void CopyFiles(List<string> missingDirectoryFiles, string sourceDirectory, string targetDirectory)
+        {
+            foreach (string missingFiles in missingDirectoryFiles)
+            {
+                if (regexSongFileEndings.IsMatch(missingFiles))
+                {
+                    string fullFilePath = sourceDirectory + missingFiles;
+                    FileInfo songToMove = new FileInfo(fullFilePath);
+                    songToMove.CopyTo((targetDirectory + missingFiles));
+                    Console.WriteLine(missingFiles);
+                }
+            }
+
+        }
+    }
+}
---
+++ MoveFilesBackup/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace MoveFilesBackup
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            List<string> sourceDirectoryFiles = new List<string>();
+            List<string> targetDirectoryFiles = new List<string>();
+
+            string sourceDirectory = @"D:\Colin\Backup\Music\Albums";
+            string targetDirectory = @"C:\Users\Colin\Music\Albums";
+
+            DirectoryMusic backupMusicFilesOperations = new DirectoryMusic();
+
+            sourceDirectoryFiles = backupMusicFilesOperations.WalkDirectory(sourceDirectory);
+            targetDirectoryFiles = backupMusicFilesOperations.WalkDirectory(targetDirectory);
+
+            List<string> notInTargetLocation = sourceDirectoryFiles.Except(targetDirectoryFiles).ToList();
+
+            Console.WriteLine("Creating new directories in target location");
+
+            backupMusicFilesOperations.CreateFolders(notInTargetLocation, targetDirectory);
+
+            Console.WriteLine("\nMoving the files over now");
+
+            backupMusicFilesOperations.CopyFiles(notInTargetLocation, sourceDirectory, targetDirectory);
+
+            Console.WriteLine("\nBoth directories should be the same now");
+            Console.ReadLine();
+        }
+    }
+}
